9d8abab56d6b76dc828d5034964abc7d32b6ffeb
jeoungGyy <limjungkoro@gmail.com>
2017-12-20 18:45:54 +0900

    React Default

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+    render(){
+
+        return (
+            <h1>React 서버구동 기본파일</h1>
+        );
+    }
+}
+
+export default App;
